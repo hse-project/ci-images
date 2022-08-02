561af389e160c5be58eba326d486eeb4c941bd35
Tristan Partin <tristan@partin.io>
2022-08-02 14:22:03 -0500

    Add initial version of images
    Included are Fedora 36, Debian 11 for s390x, Ubuntu
    18.04/20.04/22.04, and Almalinux 8/9.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

---
+++ scripts/test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -x
+set -e
+
+cross_args=
+if [ -n "${CROSS_ARCH}" ]; then
+    cross_args="--cross-file hse/cross/${CROSS_ARCH}.ini --cross-file hse/cross/common.ini"
+fi
+
+# shellcheck disable=SC2086
+meson builddir hse --fatal-meson-warnings --werror ${cross_args} \
+    -Dbindings=all -Dtools=enabled
+ninja -C builddir
+
+if [ -z "${CROSS_ARCH}" ]; then
+    meson test -C builddir --setup=ci
+fi
